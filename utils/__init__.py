8bc4b6cd442602ba640dd5207e4cbe7a8e310eba
Andrewliao18 <yl8520@nyu.edu>
2025-07-02 19:22:16 -0400

    added optimizing for GPU function

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,7 +6,7 @@ Utility functions for data loading, visualization, and GPU optimization.
 
 from .data_utils import load_room_data, save_room_data
 from .plot_utils import plot_training_progression, plot_room_layout
-from .gpu_utils import detect_optimal_device, get_gpu_info
+from .gpu_utils import detect_optimal_device, get_gpu_info, optimize_for_gpu
 
 __all__ = [
     'load_room_data',
